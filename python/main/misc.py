7f3221330c00c6f6a8adf0f25ac1bd2b75f7892b
takelley1 <hxyz@protonmail.com>
2020-05-04 02:57:56 -0400

    Rework docstrings for misc.py
    Signed-off-by: takelley1 <hxyz@protonmail.com>

--- python/main/misc.py
+++ python/main/misc.py
@@ -2,45 +2,41 @@ import time
 import logging as log
 import random as rand
 
-import pyautogui as pag
-
 
 def rand_val(rmin=0, rmax=100):
-    """Gets a random integer between two values. This is used for waiting
-    a random period of time inside of other functions. The input is in
-    miliseconds, but the output is in seconds.
+    """Gets a random integer between two values. Input is in miliseconds but
+    output is in seconds. For example, if this function generates a random value
+    of 391, it will return a value of 0.391.
 
     Arguments:
 
-        rmin (default = 0): The minimum period of time to wait for.
-        rmax (default = 100): The maximum period of time to wait for.
+        rmin (default = 0)  : The minimum number of miliseconds.
+        rmax (default = 100): The maximum number of miliseconds.
 
     Returns:
         
-        Returns a random float."""
+        Returns a FLOAT."""
 
-    log.debug('Generating random value between ' + str(rmin) + ' and ' + str(
-            rmax))
     randval = float(rand.randint(rmin, rmax)) / 1000
+    log.debug('Got random value of ' + str(randval) + ' .')
     return randval
 
 
 def sleep_rand(rmin=0, rmax=100):
-    log.debug('Sleeping between ' + str(rmin) + ' and ' + str(rmax) +
-              ' miliseconds.')
-    """Does nothing for a random period of time. Argument values are in
-    miliseconds.
+    """Does nothing for a random period of time.
 
     Arguments:
 
-        rmin (default = 0): The minimum time to wait.
-        rmax (default = 100): The maximum time to wait.
+        rmin (default = 0)  : The minimum number of miliseconds to wait.
+        rmax (default = 100): The maximum number of miliseconds to wait.
 
     Returns:
 
-        Always returns 0."""
+        Returns 0 after sleeping."""
 
-    time.sleep(float(rand.randint(rmin, rmax)) / 1000)
+    sleeptime = rand_val(rmin, rmax)
+    log.debug('Sleeping for ' + str(sleeptime) + ' seconds.')
+    time.sleep(float(sleeptime))
     return 0
 
 
