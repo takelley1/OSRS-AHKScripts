205a808b035ebf57b57e9946bb33baa55405add7
takelley1 <hxyz@protonmail.com>
2020-05-04 21:32:12 -0400

    Flesh out initial testing framework
    Signed-off-by: takelley1 <hxyz@protonmail.com>

--- python/tests/test_vision.py
+++ python/tests/test_vision.py
@@ -1,3 +1,4 @@
+import logging as log
 import os
 import time
 import sys
@@ -5,26 +6,49 @@ from unittest import TestCase
 from python.main import vision
 sys.setrecursionlimit(9999)
 
+log.basicConfig(format='%(asctime)s -- %(filename)s.%(funcName)s - %(message)s',
+                level='DEBUG')
+os.system('pkill feh &')
 
 class TestWait_for_image(TestCase):
 
-    def test_wait_for_image_orient(self):
-        os.system('sxiv -z 100 ./tests/images/'
-                  'smithing-edgeville-cannonballs/CSAtBank2.png &')
+    def test_wait_for_image_prayers(self):
+        # Tests if the script can see the prayers icon in the main menu for
+        # establishing the script's coordinate system.
+        os.system('feh -dB black ./tests/haystacks/'
+                  'smithing-edgeville-cannonballs/edgeville-bank-booth.png &')
         time.sleep(.3)
-        test_return = vision.Vision(needle='./tests/images/Orient1.PNG')
-        test_return = test_return.wait_for_image()
-        self.assertNotEqual(test_return, 1)
-        os.system('pkill -f "./tests/images/'
-                  'smithing-edgeville-cannonballs/CSAtBank2.png"')
+        test_return = vision.Vision(needle='./main/needles/menu/prayers.png')\
+            .click_image()
+        self.assertEqual(test_return, 0)
+        os.system('pkill -f "tests/haystacks/'
+                  'smithing-edgeville-cannonballs/edgeville-bank-booth.png" &')
+        time.sleep(.3)
 
-    def test_wait_for_image_bank(self):
-        os.system('sxiv -z 100 ./tests/images/'
-                  'smithing-edgeville-cannonballs/CSAtBank2.png &')
+    def test_wait_for_image_minimap_edgeville_bank_booth(self):
+        # Tests if the script can recognize when the player is standing on the
+        # correct tile in Edgeville bank by checking the minimap.
+        os.system('feh -dB black ./tests/haystacks/'
+                  'smithing-edgeville-cannonballs/edgeville-bank-booth.png &')
         time.sleep(.3)
-        test_return = vision.Vision(needle='./tests/images/needles/game-screen/'
-                                           'bank-booth-edgeville.png')
-        test_return = test_return.wait_for_image()
-        self.assertNotEqual(test_return, 1)
-        os.system('pkill -f "./tests/images/'
-                  'smithing-edgeville-cannonballs/CSAtBank2.png"')
+        test_return = vision.Vision(needle='./main/needles/minimap/'
+                                           'edgeville-bank-booth.png') \
+            .click_image()
+        self.assertEqual(test_return, 0)
+        os.system('pkill -f "tests/haystacks/'
+                  'smithing-edgeville-cannonballs/edgeville-bank-booth.png" &')
+        time.sleep(.3)
+
+    def test_wait_for_image_game_screen_edgeville_bank_booth(self):
+        # Tests if the script can recognize the Edgeville bank booth in the
+        # game screen when the player is standing on the correct tile.
+        os.system('feh -dB black ./tests/haystacks/'
+                  'smithing-edgeville-cannonballs/edgeville-bank-booth.png &')
+        time.sleep(.3)
+        test_return = vision.Vision(needle='./main/needles/game-screen/'
+                                           'edgeville-bank-booth.png') \
+            .click_image()
+        self.assertEqual(test_return, 0)
+        os.system('pkill -f "tests/haystacks/'
+                  'smithing-edgeville-cannonballs/edgeville-bank-booth.png" &')
+        time.sleep(.3)
