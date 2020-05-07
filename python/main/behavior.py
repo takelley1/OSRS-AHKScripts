6f00b576b88de8eb88577d1be64e2376da5d59ce
T. Austin Kelley <hxyz@protonmail.com>
2020-05-07 11:53:35 -0400

    Create behavior.py

---
+++ python/main/behavior.py
@@ -0,0 +1,25 @@
+
+
+# TODO
+
+def chat(context)
+  if context == 'smelting'
+    type 'option 1'
+    type 'option 2'
+  
+  elif context == 'blablabla'
+  
+def move_camera()
+    down arrow roll
+    left arrow roll
+    right arrow roll
+ 
+def check_experience(skill)
+
+def open_menu_rand()
+
+def open_menu()
+
+def logout()
+
+def switch_worlds()
