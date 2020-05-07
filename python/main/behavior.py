7cda978756b3a4f1b6aff24899ab39fd5f66fc3b
T. Austin Kelley <hxyz@protonmail.com>
2020-05-07 12:16:42 -0400

    Outline basic functions of behavior.py

--- python/main/behavior.py
+++ python/main/behavior.py
@@ -1,4 +1,5 @@
-
+from python.main import input
+from python.main import vision
 
 # TODO
 
@@ -9,17 +10,36 @@ def chat(context)
   
   elif context == 'blablabla'
   
-def move_camera()
-    down arrow roll
-    left arrow roll
+def move_camera_rand(chance=3,down_min=200,down_max=2000)
+    down_arrow_roll = rand.randint(1, chance)
+    if down_arrow_roll == chance:
+       input.keyDown('Down')
+       time.sleep(float(rand.randint(down_min, down_max)))
+       input.keyUp('Down')
+    left
     right arrow roll
  
 def check_experience(skill)
 
 def open_menu_rand()
 
-def open_menu()
+def open_menu(menu)
+  if menu == 'prayers'
+    menu_open = vsion.click_image(needle='./prayers')
+    if menu_open == 0:
+      menu_open = vision.wait_for_image(needle='prayer_x')
+        if menu_open == 0:
+          return 0
+        else:
+          return 1
+    else:
+      return 1
+  elif menu == 'inventory'
+  elif menu == 'magic'
+  elif menu == 'equipment'
+  elif menu == 'logout'
 
 def logout()
+  open_menu(logout)
 
 def switch_worlds()
