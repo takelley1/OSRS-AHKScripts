4a4764eb99ae60da05ce1828d6633ec571d0d4bb
takelley1 <hxyz@protonmail.com>
2020-05-07 21:02:42 -0400

    Add screenshot.py for quickly capturing client window
    Signed-off-by: takelley1 <hxyz@protonmail.com>

---
+++ python/main/screenshot.py
@@ -0,0 +1,27 @@
+import os
+import pyautogui as pag
+from python.main import vision as vis
+
+
+def main():
+    """Takes and automatically crops a screenshot to capture the OSRS client
+    window. Useful for running on demand manually."""
+    os.chdir('/home/austin/OSRS-AHKScripts/python/tests')
+    target_image = vis.Vision(needle='../main/needles/menu/prayers.png')
+    target_image = target_image.mlocate(loctype='center')
+
+    (x, y) = target_image
+    x -= 709
+    y -= 186
+
+    pag.screenshot('screenshot.tmp.png', region=(x, y, 765, 503))
+
+    os.system('pngcrush -s '
+              'screenshot.tmp.png haystack_$(date +%Y-%m-%d_%H:%M:%S.png)'
+              '&& notify-send -u low "screenshot taken" '
+              '&& rm screenshot.tmp.png')
+    return
+
+
+if __name__ == '__main__':
+    main()
