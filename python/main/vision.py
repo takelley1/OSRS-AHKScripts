7b9542b53863e770dabdc8dd19b9f32c5f6578fd
takelley1 <hxyz@protonmail.com>
2020-05-10 01:17:47 -0400

    Add orient() and drop_all_item() functions to vision.py
    Signed-off-by: takelley1 <hxyz@protonmail.com>

--- python/main/vision.py
+++ python/main/vision.py
@@ -8,6 +8,66 @@ from python.main import input
 
 sys.setrecursionlimit(9999)
 
+# These are constants.
+# Width and height of the entire game client.
+CLIENT_WIDTH = 765
+CLIENT_HEIGHT = 503
+
+# Width and height of the inventory screen, in pixels.
+INV_WIDTH = 190
+INV_HEIGHT = 265
+
+# TODO: game screen coords
+# Width and height of just the game screen in the game client.
+GAME_SCREEN_WIDTH = 0
+GAME_SCREEN_HEIGHT = 0
+
+
+# TODO: change orient to something other than the prayer icon
+def orient():
+    """
+    Creates objects containing useful game coordinates for other
+    functions to serach through.
+    """
+    global CLIENT_HEIGHT, CLIENT_WIDTH
+    global INV_HEIGHT, INV_WIDTH
+
+    # Get the display size in pixels.
+    display_width = pag.size().width
+    display_height = pag.size().height
+
+    # Look for the prayers icon on the display. If it's found, use its
+    #   location within the game client to determine the coordinates of
+    #   the game client relative to the display's coordinates.
+    anchor = Vision(left=0, top=0,
+                    width=display_width,
+                    height=display_height) \
+        .wait_for_image(needle='./main/needles/main-menu/prayers.png')
+
+    # The wait_for_image function returns a tuple with a few vars we
+    #   don't need.
+    (client_left, client_top, unused_var1, unused_var2) = anchor
+
+    # The left corner of the game client is 709 pixels to the left of
+    #   the prayers icon
+    client_left -= 709
+    client_top -= 186
+
+    # Now we can create an object with the game client's X and Y
+    #   coordinates. This will allow other functions to search for
+    #   needles within the "client" object's coordinates, rather than
+    #   within the entire display's coordinates, which is much faster.
+    client = Vision(left=client_left, width=CLIENT_WIDTH,
+                    top=client_top, height=CLIENT_HEIGHT)
+
+    # Create another object for the player's inventory.
+    inv_left = client_left + 555
+    inv_top = client_top + 220
+    inv = Vision(left=inv_left, top=inv_top,
+                 width=INV_WIDTH, height=INV_HEIGHT)
+
+    return client, inv
+
 
 class Vision:
     """
@@ -180,7 +240,10 @@ class Vision:
         return 1
 
     def click_image(self, needle, button='left', conf=0.95,
-                    loop_num=25, loop_sleep_min=10, loop_sleep_max=1000):
+                    loop_num=25, loop_sleep_min=10, loop_sleep_max=1000,
+                    click_sleep_before_min=0, click_sleep_before_max=500,
+                    click_sleep_after_min=0, click_sleep_after_max=500,
+                    move_duration_min=50, move_duration_max=1500):
         """
         Moves the mouse to the provided needle image and clicks on
         it. If a haystack is provided, searches for the provided needle
@@ -188,8 +251,9 @@ class Vision:
         is not provided, searches within the entire display.
 
         Arguments:
+            button: The mouse button to use when clicking on the needle.
+
             needle: See mlocate()'s docstring.
-            loctype (default = 'regular'): see mlocate()'s docstring.
             conf (default = 0.95): See mlocate()'s docstring.
 
             loop_num (default = 10): See wait_for_image()'s docstring.
@@ -213,10 +277,34 @@ class Vision:
         if target_image == 1:
             return 1
         else:
-            (x, y, w, h) = target_image
+            (left, top, width, height) = target_image
             # Randomize the location the pointer will move to using the
             #   dimensions of needle image.
-            input.move_to(x, y, xmin=0, xmax=w, ymin=0, ymax=h)
+            input.move_to(left, top,
+                          xmin=0, xmax=width,
+                          ymin=0, ymax=height,
+                          duration_min=move_duration_min,
+                          duration_max=move_duration_max)
+
             log.debug('Clicking on ' + str(needle) + '.')
-            input.click(button=button)
+
+            input.click(button=button,
+                        sleep_before_min=click_sleep_before_min,
+                        sleep_before_max=click_sleep_before_max,
+                        sleep_after_min=click_sleep_after_min,
+                        sleep_after_max=click_sleep_after_max)
             return 0
+
+    def drop_all_item(self, needle):
+        item_to_drop = self.wait_for_image(needle=needle)
+        while item_to_drop != 1:
+            pag.keyDown('shift')
+            item_to_drop = self.click_image(loop_num=2,
+                                            click_sleep_before_min=0,
+                                            click_sleep_before_max=100,
+                                            move_duration_min=10,
+                                            move_duration_max=500,
+                                            needle=needle)
+            pag.keyUp('shift')
+            misc.sleep_rand(100, 1000)
+        return 0
