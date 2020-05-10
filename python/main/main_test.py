23214b6c88f05ce3b15a9083341903b0cf4bb6a0
takelley1 <hxyz@protonmail.com>
2020-05-09 22:46:21 -0400

    Expand and revamp main cannonball smelter, minimap no longer used
    Signed-off-by: takelley1 <hxyz@protonmail.com>

--- python/main/main_test.py
+++ python/main/main_test.py
@@ -5,6 +5,8 @@ import time
 import psutil
 import pyautogui as pag
 
+from python.main import behavior as behav
+from python.main import input
 from python.main import vision as vis
 
 # This file is a full regression test of the bot that intends to test every
@@ -13,7 +15,7 @@ from python.main import vision as vis
 # Intended for Linux with the feh image viewer.
 
 log.basicConfig(format='%(asctime)s -- %(filename)s.%(funcName)s - %(message)s'
-                , level='DEBUG')
+                , level='INFO')
 
 # These are constants.
 # The width and height of the client in pixels does not change.
@@ -45,17 +47,9 @@ def test_cannonball_smelter():
     kill('feh')
     sub.Popen(["feh", "./tests/haystacks/"
                       "smithing-edgeville-cannonballs/"
-                      "edgeville-bank-booth.png"])
+                      "edgeville-bank-booth-01.png"])
     time.sleep(interval)
     # -------------------------------------------------------------------------
-
-    # Orient client to establish coordinate space.
-    # Read config file and get client resolution.
-    # with open('./config.yaml') as f:
-    # config = yaml.safe_load(f)
-
-    # client_xmax = config['client_width']
-    # client_ymax = config['client_height']
 
     # Get initial display size.
     screen_width = pag.size().width
@@ -67,7 +61,7 @@ def test_cannonball_smelter():
     anchor = vis.Vision(left=0, top=0,
                         width=screen_width,
                         height=screen_height) \
-        .wait_for_image(needle='./main/needles/menu/prayers.png')
+        .wait_for_image(needle='./main/needles/main-menu/prayers.png')
 
     # The wait_for_image function returns a tuple with a few vars we don't
     #   need.
@@ -86,7 +80,8 @@ def test_cannonball_smelter():
 
     # Click on the bank booth.
     bank_booth = client.click_image(needle='./main/needles/game-screen/'
-                                           'edgeville-bank-booth-01.png')
+                                           'edgeville-bank-booth-03.png',
+                                    conf=0.995)
     if bank_booth == 1:
         raise RuntimeError('Could not find bank booth!')
 
@@ -126,7 +121,7 @@ def test_cannonball_smelter():
     time.sleep(interval)
     # -------------------------------------------------------------------------
 
-    # Select the withdraw option in right-click menu.
+    # Select the withdraw option in right-click main-menu.
     withdraw_steel_bars = client.click_image(needle='./main/needles/buttons/'
                                                     'right-click-'
                                                     'withdraw-all.png')
@@ -138,7 +133,7 @@ def test_cannonball_smelter():
     time.sleep(interval)
     sub.Popen(["feh", "./tests/haystacks/"
                       "smithing-edgeville-cannonballs/"
-                      "edgeville-furnace-01.png"])
+                      "edgeville-bank-window-02.png"])
     time.sleep(interval)
     # -------------------------------------------------------------------------
 
@@ -149,16 +144,86 @@ def test_cannonball_smelter():
 
     # The left edge of the inventory is about 555 pixels to the right of the
     #   left edge of the game client.
-    inv_left = client_left + 555
-    inv_top = client_top + 220
+    #inv_left = client_left + 555
+    #inv_top = client_top + 220
 
-    inv = vis.Vision(left=inv_left, top=inv_top,
-                     width=INV_WIDTH, height=INV_HEIGHT)
-    steel_bars_in_inventory = inv.wait_for_image(needle='./main/needles/'
-                                                        'items/steel-bar.png')
-    if steel_bars_in_inventory == 1:
-        raise RuntimeError('Timed out waiting for steel bars to show up'
-                           'in inventory!')
+    #inv = vis.Vision(left=inv_left, top=inv_top,
+                     #width=INV_WIDTH, height=INV_HEIGHT)
+    #steel_bars_in_inventory = inv.wait_for_image(needle='./main/needles/'
+                                                        #'items/steel-bar.png')
+    #if steel_bars_in_inventory == 1:
+        #raise RuntimeError('Timed out waiting for steel bars to show up'
+                           #'in inventory!')
+
+    # Close the bank window
+    bank_window_close = client.click_image(needle='./main/needles/'
+                                           'buttons/bank-window-close.png')
+    # -------------------------------------------------------------------------
+    kill('feh')
+    time.sleep(interval)
+    sub.Popen(["feh", "./tests/haystacks/"
+                      "smithing-edgeville-cannonballs/"
+                      "edgeville-bank-booth-06.png"])
+    time.sleep(interval)
+    # -------------------------------------------------------------------------
+
+    furnace = client.click_image(needle='./main/needles/game-screen/'
+                                        'edgeville-furnace-from-bank.png')
+
+    # -------------------------------------------------------------------------
+    kill('feh')
+    time.sleep(interval)
+    sub.Popen(["feh", "./tests/haystacks/"
+                      "smithing-edgeville-cannonballs/"
+                      "edgeville-furnace-03.png"])
+    time.sleep(interval)
+    # -------------------------------------------------------------------------
+
+    # Press spacebar to begin smelting the cannonballs.
+    begin_smelting = client.wait_for_image(needle='./main/needles/chat-menu/'
+                                                  'smelting.png')
+    if begin_smelting == 1:
+        raise RuntimeError('Could not find smelting.png chat menu!')
+
+    input.keypress('space')
+
+    # -------------------------------------------------------------------------
+    kill('feh')
+    time.sleep(interval)
+    sub.Popen(["feh", "./tests/haystacks/"
+                      "smithing-edgeville-cannonballs/"
+                      "edgeville-furnace-04.png"])
+    time.sleep(interval)
+    # -------------------------------------------------------------------------
+
+    # Smelting has completed when the bottom half of the inventory is empty.
+    # Continually check for this, waiting at least 10 minutes.
+    done_smelting = client.wait_for_image(loop_num=600,
+                                          loop_sleep_min=1000,
+                                          loop_sleep_max=5000,
+                                          conf=0.98,
+                                          needle='./main/needles/main-menu/'
+                                                 'inventory-empty-'
+                                                 'lower-half.png')
+
+    # Even if wait_for_image times out, return to the bank.
+    if done_smelting == 1 or done_smelting != 1:
+        bank = client.click_image(needle='./main/needles/game-screen'
+                                         '/edgeville-bank-from-furnace-01.png')
+
+        # ---------------------------------------------------------------------
+        kill('feh')
+        time.sleep(interval)
+        sub.Popen(["feh", "./tests/haystacks/"
+                          "smithing-edgeville-cannonballs/"
+                          "edgeville-furnace-04.png"])
+        time.sleep(interval)
+        # ---------------------------------------------------------------------
+
+        # Deposit cannonballs once bank window appears
+
+# Small chance to do nothing before returning to bank.
+# behav.wait_rand(chance=10, wait_min=10000, wait_max=60000)
 
 
 # -----------------------------------------------------------------------------
