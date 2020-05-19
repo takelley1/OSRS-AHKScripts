af7bf49e38cfacbf52681b27bf71e9f96ac1327e
takelley1 <hxyz@protonmail.com>
2020-05-18 21:58:38 -0400

    Add basic login script for faster testing
    Signed-off-by: takelley1 <hxyz@protonmail.com>

---
+++ scripts/login.py
@@ -0,0 +1,27 @@
+import pyautogui as pag
+
+from ocvbot import vdisplay, input, misc
+
+
+def main():
+    """
+    Automatically logs the client in using credentials specified in a file.
+    """
+
+    logged_out = vdisplay.click_image(needle='./needles/orient-logged-out.png')
+    if logged_out == 1:
+        raise RuntimeError("Cannot find client!")
+    else:
+        misc.sleep_rand(200, 3000)
+        input.keypress('enter')
+        misc.sleep_rand(200, 3000)
+        pag.typewrite(open('username', 'r').read())
+        misc.sleep_rand(200, 3000)
+        input.keypress('tab')
+        pag.typewrite(open('password', 'r').read())
+        misc.sleep_rand(200, 3000)
+        input.keypress('enter')
+
+
+if __name__ == '__main__':
+    main()
