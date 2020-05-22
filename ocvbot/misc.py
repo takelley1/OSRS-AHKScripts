86b9a880d3a6dd85458235be486fb5fe5af07cdf
takelley1 <hxyz@protonmail.com>
2020-05-21 22:28:44 -0400

    Reformat docstrings in misc.py
    Signed-off-by: takelley1 <hxyz@protonmail.com>

--- ocvbot/misc.py
+++ ocvbot/misc.py
@@ -1,21 +1,20 @@
 import random as rand
-import sys
 import time
-
-sys.setrecursionlimit(9999)
 
 
 def rand_seconds(rmin=0, rmax=100):
-    """Gets a random integer between two values. Input arguments are in
+    """
+    Gets a random integer between two values. Input arguments are in
     miliseconds but output is in seconds. For example, if this function
     generates a random value of 391, it will return a value of 0.391.
 
-    Arguments:
-        rmin (default = 0)  : The minimum number of miliseconds.
-        rmax (default = 100): The maximum number of miliseconds.
-
+    Args:
+        rmin (int): The minimum number of miliseconds, default is 0.
+        rmax (int): The maximum number of miliseconds, default
+                    is 100.
     Returns:
-        Returns a float."""
+        Returns a float.
+    """
 
     randval = rand.randint(rmin, rmax)
     randval = float(randval / 1000)
@@ -24,15 +23,19 @@ def rand_seconds(rmin=0, rmax=100):
 
 
 def sleep_rand(rmin=0, rmax=100):
-    """Does nothing for a random period of time. Input arguments are in
+    """
+    Does nothing for a random period of time. Input arguments are in
     miliseconds.
 
-    Arguments:
-        rmin (default = 0)  : The minimum number of miliseconds to wait.
-        rmax (default = 100): The maximum number of miliseconds to wait.
+    Args:
+        rmin (int): The minimum number of miliseconds to wait, default
+                    is 0.
+        rmax (int): The maximum number of miliseconds to wait, default
+                    is 100.
 
     Returns:
-        Always returns 0."""
+        Always returns 0.
+    """
 
     sleeptime = rand_seconds(rmin, rmax)
     # log.debug('Sleeping for ' + str(sleeptime) + ' seconds.')
