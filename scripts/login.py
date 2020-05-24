95656d3374c1efc3f3aafce7b59c7589b55fd5b8
takelley1 <hxyz@protonmail.com>
2020-05-23 21:10:09 -0400

    Bring login.py script to a working state
    Signed-off-by: takelley1 <hxyz@protonmail.com>

--- scripts/login.py
+++ scripts/login.py
@@ -1,27 +1,36 @@
 import pyautogui as pag
 
-from ocvbot import input, misc
-from ocvbot.vision import vdisplay
+from ocvbot import input, misc, vision
 
 
 def main():
     """
     Automatically logs the client in using credentials specified in a file.
     """
+    vision.init_vision()
+    from ocvbot.vision import vdisplay
 
     logged_out = vdisplay.click_image(needle='./needles/orient-logged-out.png')
     if logged_out == 1:
         raise RuntimeError("Cannot find client!")
     else:
-        misc.sleep_rand(200, 3000)
+        misc.sleep_rand(800, 3000)
         input.keypress('enter')
-        misc.sleep_rand(200, 3000)
+        misc.sleep_rand(800, 3000)
         pag.typewrite(open('username', 'r').read())
-        misc.sleep_rand(200, 3000)
-        input.keypress('tab')
+        misc.sleep_rand(800, 3000)
         pag.typewrite(open('password', 'r').read())
-        misc.sleep_rand(200, 3000)
+        misc.sleep_rand(800, 3000)
         input.keypress('enter')
+        postlogin = vdisplay.click_image(needle='./needles/'
+                                                'orient-postlogin.png',
+                                         conf=0.8,
+                                         loop_num=10,
+                                         loop_sleep_max=5000)
+        if postlogin != 1:
+            return 0
+        else:
+            raise RuntimeError("Cannot log in!")
 
 
 if __name__ == '__main__':
