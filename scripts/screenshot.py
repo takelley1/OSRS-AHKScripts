3bdc5123f8e9162adc380efa7d869150b724f353
takelley1 <hxyz@protonmail.com>
2020-05-24 23:03:58 -0400

    Add docstring, comments, minor fixes to screenshot.py Changed the "debug" argument to a boolean, made all invocations of pngcrush direct all output to /dev/null, fixed typo with haystack_ output file name if  debug is set to False.
    Signed-off-by: takelley1 <hxyz@protonmail.com>

--- scripts/screenshot.py
+++ scripts/screenshot.py
@@ -6,10 +6,27 @@ log.basicConfig(format='%(asctime)s -- %(filename)s.%(funcName)s - %(message)s'
                 , level='INFO')
 
 
-def main(debug=0):
+def main(debug=False):
     """
-    Takes a screenshot of the OSRS
-    client window. Useful for running on demand manually.
+    Takes a screenshot of the OldSchool Runescape client window.
+
+    Automatically censors player's username.
+
+    Optionally overlays the coordinates of Vision objects, which can be
+    useful for debugging. This function is designed to be run manually.
+
+
+    Args:
+        debug (bool): Whether the function will perform extra processing
+                      to overlay rectanges cooresponding to Vision
+                      object coordinates on the screenshot, which takes
+                      a second or two, default is False. If set to
+                      False, the function produces an image called
+                      haystack_$(date +%Y-%m-%d_%H:%M:%S).png in the
+                      current directory.
+
+    Returns:
+        Always returns 0.
     """
 
     log.info('Initializing')
@@ -20,29 +37,36 @@ def main(debug=0):
     from ocvbot.vision import vclient_left, vclient_top, \
         CLIENT_HEIGHT, CLIENT_WIDTH, client_status
 
-    if debug == 0:
+    if debug is False:
         pag.screenshot('.screenshot.tmp.png', region=(vclient_left,
                                                       vclient_top,
                                                       CLIENT_WIDTH,
                                                       CLIENT_HEIGHT))
         log.info('Processing screenshot')
         if client_status == 'logged_in':
+            # If the client is logged in, censor the player's username
+            #   by drawing a black box over it with ImageMagick.
             os.system('pngcrush -s '
-                      '.screenshot.tmp.png .screenshot.tmp2.png'
+                      '.screenshot.tmp.png .screenshot.tmp2.png '
+                      '&>/dev/null '
                       '&& convert .screenshot.tmp2.png '
                       '-fill black '
                       '-draw "rectangle 7 458 190 473" '
-                      'haystack_$(date +%Y-%m-%d_%H:%M:%S.png)client.png'
+                      'haystack_$(date +%Y-%m-%d_%H:%M:%S).png'
                       '&& rm .screenshot.tmp*.png'
                       '&& notify-send -u low "Screenshot taken"')
         elif client_status == 'logged_out':
             os.system('pngcrush -s '
                       '.screenshot.tmp.png '
-                      'haystack_$(date +%Y-%m-%d_%H:%M:%S.png)client.png'
+                      'haystack_$(date +%Y-%m-%d_%H:%M:%S).png '
+                      '&>/dev/null '
                       '&& rm .screenshot.tmp*.png'
                       '&& notify-send -u low "Screenshot taken"')
+        else:
+            raise RuntimeError("Could not interpret client_status var!")
 
-    elif debug == 1:
+    elif debug is True:
+        # Import all the Vision objects to overlay onto the screenshot.
         from ocvbot import DISPLAY_WIDTH, DISPLAY_HEIGHT
         from ocvbot.vision import \
             vinv_left, vinv_top, \
@@ -67,6 +91,8 @@ def main(debug=0):
         os.system('pngcrush -s .screenshot.tmp2.png .screenshot.tmp.png'
                   '&>/dev/null')
 
+        # Create a separate edited file for each Vision object, as some
+        #   of the objects' coordinates overlap.
         log.info('Creating client.png')
         os.system('convert .screenshot.tmp.png '
                   '-fill red '
@@ -149,9 +175,9 @@ def main(debug=0):
 
         os.system('rm .screenshot.tmp*.png && '
                   'notify-send -u low "Debug screenshots taken!"')
-        return
+        return 0
 
-    return
+    return 0
 
 
 if __name__ == '__main__':
